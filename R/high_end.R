efd7c492318f0a788eb643f6e546af73568146af
unknown <sriharitn@gmail.com>
2019-04-01 00:54:07 -0400

    add data

---
+++ R/high_end.R
@@ -0,0 +1,10 @@
+#' \% of High End Customers Surviving Years 0 - 12.
+#'
+#' A dataset containing cusomer retention for High End customers.
+#'
+#' @format A data frame 13 observatios:
+#' \describe{
+#'   \item{high_end}{\% customers high end Retained from 0 - 12 years}
+#'}
+#' @references {Fader  PS and Hardie BGS (2007), How to project customer retention. Volume 21, Issue 1. Journal of Interactive Marketing}
+"high_end"
---
+++ R/regular_end.R
@@ -0,0 +1,10 @@
+#' \% of Regular Customers Surviving Years 0 - 12.
+#'
+#' A dataset containing cusomer retention for Regular customers
+#'
+#' @format A data frame 13 observatios:
+#' \describe{
+#'   \item{regular_end}{\% customers regular end Retained from 0 - 12 years}
+#'}
+#' @references {Fader  PS and Hardie BGS (2007), How to project customer retention. Volume 21, Issue 1. Journal of Interactive Marketing}
+"regular_end"
