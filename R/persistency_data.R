016d74572e0631b0b41a7916b491a35c7760ddbd
Jaganathan Srihari <U044338@dir.ucb-group.com>
2019-04-03 01:14:12 -0400

    version 0.2

---
+++ R/persistency_data.R
@@ -0,0 +1,16 @@
+#' \% of patients persistency by different theraputic class.
+#'
+#' A dataset containing persistency of patients in different therapeutic classes.
+#'
+#' @format A data frame 334 observatios and 3 variables:
+#' \describe{
+#'   \item{Data}{ Type of Dataset\, example Hypertension\, Epilepsy}
+#'   \item{Time}{Time Period}
+#'   \item{Value}{\% Patients retained by time}
+#'}
+#' @docType data
+#' @keywords datasets
+#' @name persistency_data
+#' @usage data(persistency_data)
+#' @format A data frame with 334 rows and 3 variables
+NULL
